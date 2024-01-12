c0ab7641d06d22e5e826148e71566555cdab79ad
RichardJanebo <richardjanebo@gmail.com>
2024-01-11 23:07:42 -0300

    inserindo js

--- javas.js
+++ javas.js
@@ -7,7 +7,28 @@ function clicar(){
         let li = document.createElement("li")
         li.innerHTML= dados.value
         lista.appendChild(li)
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
     }
     dados.value = ''
+    saveData()
 }
-let span 
+lista.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+        saveData()
+    }
+},false)
+
+function saveData() {
+    localStorage.setItem("data", lista.innerHTML)
+
+}
+function showTask(){
+    lista.innerHTML = localStorage.getItem("data")
+}
+showTask()
